febe0e35db8818af5c6c0aba709dcb5fa48d5676
Peter Mulu Kioko <peter.kioko@student.moringaschool.com>
2022-06-22 14:23:24 +0300

    added comment section interactivity

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
-
 const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2Cethereum%2Cdogecoin%2Ctether%2Ccardano%2Csolana%2Cpolkadot%2Cdai%2Ctron%2Cavalanch%2Clitecoin%2Cpolygon%2Cbnb&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 document.addEventListener('DOMContentLoaded',function(){
+  
   getPrices()
+  workComment()
 })
 let element=[]
 function getPrices(){
@@ -42,4 +43,15 @@ function tableGenerator(cryptos){
   const KryptoData = document.getElementById('data')
   KryptoData.innerHTML= krypt
 }
-
+function workComment(){
+  let post= document.getElementById("post");
+post.addEventListener("click", function(){
+    let commentBoxValue= document.getElementById("comment-box").value;
+ 
+    let li = document.createElement("li");
+    let text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered").appendChild(li);
+ 
+});
+}
